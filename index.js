f6afdd463285bf99a2fc32cb38e18cfb0663d428
junior-programmer-myanmar <akmm3131@gmail.com>
2022-01-01 21:06:51 +0630

    search-form finished

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ document.querySelector('#nav-close').onclick = () => {
     navbar.classList.remove('active')
 }
 
+
+
+let searchForm  = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.add('active')
+}
+
+
+document.querySelector('#close-search').onclick = () => {
+    searchForm.classList.remove('active')
+}
